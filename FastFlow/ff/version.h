65d3d9f487a01a4f9e3bbcfb6d25ea79844169c5
0Alic <andrealisi.12lj@gmail.com>
2018-03-09 18:11:57 +0100

    removed files

--- FastFlow/ff/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef FF_VERSION_HPP
-#define FF_VERSION_HPP
-
-#define FF_MAJOR_VERSION 2
-#define FF_MINOR_VERSION 1
-#define FF_BETA_VERSION  0
-#define FF_VERSION "2.1.0"
-
-#endif /* FF_VERSION_HPP */
